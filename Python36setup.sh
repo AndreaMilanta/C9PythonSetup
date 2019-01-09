8c0c66bf53e62f890903bd7a647b4e914d5b6b9a
Andrea Milanta <andrea.milanta@gmail.com>
2019-01-09 12:18:39 +0000

    added script

---
+++ Python36setup.sh
@@ -0,0 +1,20 @@
+#
+# NOTE: this file must be given execution rights with the command
+#
+#       chmod +x Python36setup.sh
+#
+# NOTE: Once you have executed the file, open a new bash
+#
+
+#update
+sudo yum -y update
+sudo yum -y install python36 # Installs Python 3.6.
+
+#setup aliases
+echo -e "\nalias pip='sudo /usr/local/bin/pip3.6'" >> ~/.bashrc
+echo -e "\nalias python='python36'" >> ~/.bashrc
+source ~/.bashrc
+. ~/.bashrc
+
+#upgrade pip
+sudo /usr/local/bin/pip3.6 install --upgrade pip
